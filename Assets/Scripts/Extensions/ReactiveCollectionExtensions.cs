1d85f6fb93f67ad3e5b50793da0b0c24ac155a1e
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-12-15 19:27:41 +0300

    Add reactive collection to string extension method

---
+++ Assets/Scripts/Extensions/ReactiveCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UniRx;
+
+public static class ReactiveCollectionExtensions
+{
+    public static string ToString<T>(this ReactiveCollection<T> list)
+    {
+        var result = string.Join(", ", list);
+
+        return result;
+    }
+}
