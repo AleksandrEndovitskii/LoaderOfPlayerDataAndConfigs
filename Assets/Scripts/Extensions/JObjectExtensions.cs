b2be9754f5dd13d5c2a3a460f15caf56c492be81
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-12-16 18:13:26 +0300

    Add parse to method to json object extension

---
+++ Assets/Scripts/Extensions/JObjectExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+
+public static class JObjectExtensions
+{
+    public static T ParseTo<T>(this JObject jsonObject) where T : new()
+    {
+        var configModel = new T();
+        var propertyInfos = configModel.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (var propertyInfo in propertyInfos)
+        {
+            var jsonToken = jsonObject[propertyInfo.Name];
+            System.Object value = null;
+            if (jsonToken is JArray)
+            {
+                var jArray = jsonToken.Value<JArray>();
+                value = jArray.ToObject<List<string>>();
+            }
+            else
+            {
+                value = jsonToken.ToObject<string>();
+            }
+
+            PropertyInfoExtensions.SetValue(propertyInfo, configModel, value);
+        }
+
+        return configModel;
+    }
+}
--- Assets/Scripts/Services/Configs/ResourcesConfigsLoadingService.cs
+++ Assets/Scripts/Services/Configs/ResourcesConfigsLoadingService.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
-using Models.Configs;
+﻿using Models.Configs;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 using Zenject;
@@ -19,23 +17,7 @@ namespace Services.Configs
             var configsTextAsset = Resources.Load<TextAsset>("Configs");
             var jsonString = configsTextAsset.text;
             var jsonObject = JObject.Parse(jsonString);
-            var configModel = new ConfigModel();
-            var propertyInfos = configModel.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var propertyInfo in propertyInfos)
-            {
-                var jsonToken = jsonObject[propertyInfo.Name];
-                System.Object value = null;
-                if (jsonToken is JArray)
-                {
-                    var jArray = jsonToken.Value<JArray>();
-                    value = jArray.ToObject<List<string>>();
-                }
-                else
-                {
-                    value = jsonToken.ToObject<string>();
-                }
-                PropertyInfoExtensions.SetValue(propertyInfo, configModel, value);
-            }
+            var configModel = jsonObject.ParseTo<ConfigModel>();
 
             Debug.Log($"Configs loading finished");
 
